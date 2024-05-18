06c7b389e67b295718e5d7cd8a11b337416efa56
NhatHONGUYEN <hnnhat67@gmail.com>
2024-05-18 20:34:53 +0200

    ✨ F01 : Add link for CrazeeBurger , SushiSensation and NetflixClone

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,33 +4,51 @@ export default function Projects() {
       <h1 className="text-3xl font-bold  py-8">Projects</h1>
       <div className="w-full h-full px-4 ">
         <div>
-          <img
-            className="rounded-xl  h-48 w-full object-cover"
-            src="/crazee-burger.png"
-            alt="crazee-burger"
-          />
-          <h2 className="font-bold py-5 text-xl">Crazee Burger</h2>
-          <p className="pb-10">Des burgers gourmands à portée de clic!</p>
+          <a
+            href="https://crazee-burger-eosin-rho.vercel.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              className="rounded-xl  h-48 w-full object-cover"
+              src="/crazee-burger.png"
+              alt="crazee-burger"
+            />
+            <h2 className="font-bold py-5 text-xl">Crazee Burger</h2>
+            <p className="pb-10">Des burgers gourmands à portée de clic!</p>
+          </a>
         </div>
         <div>
-          <img
-            className="rounded-xl h-48 w-full object-cover"
-            src="/Sushi-sensation.png"
-            alt="Sushi-sensation"
-          />
-          <h2 className="font-bold py-5 text-xl">Sushi Sensation</h2>
-          <p className="pb-10">
-            Résistez à la tentation... ou pas, nos sushis sont irrésistibles !
-          </p>
+          <a
+            href="https://sushi-sensation.vercel.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              className="rounded-xl h-48 w-full object-cover"
+              src="/Sushi-sensation.png"
+              alt="Sushi-sensation"
+            />
+            <h2 className="font-bold py-5 text-xl">Sushi Sensation</h2>
+            <p className="pb-10">
+              Résistez à la tentation... ou pas, nos sushis sont irrésistibles !
+            </p>
+          </a>
         </div>
         <div>
-          <img
-            className="rounded-xl h-48 w-full object-cover"
-            src="/netflix-port.png"
-            alt="netflix-port"
-          />
-          <h2 className="font-bold py-5 text-xl">Netflix clone</h2>
-          <p className="pb-10">See what's next.</p>
+          <a
+            href="https://netflix-clone-k9aeneoof-nhat-quans-projects.vercel.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              className="rounded-xl h-48 w-full object-cover"
+              src="/netflix-port.png"
+              alt="netflix-port"
+            />
+            <h2 className="font-bold py-5 text-xl">Netflix clone</h2>
+            <p className="pb-10">See what's next.</p>
+          </a>
         </div>
       </div>
     </div>
