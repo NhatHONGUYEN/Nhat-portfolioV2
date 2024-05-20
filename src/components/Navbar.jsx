278ba5313ccc53181f7190d1d5daceb0fa583875
NhatHONGUYEN <hnnhat67@gmail.com>
2024-05-20 11:14:32 +0200

    💄   F01 :NAVBAR > add hover color

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,25 +8,28 @@ export default function Navbar() {
   const node = useRef();
 
   return (
-    <div className=" z-50 flex  h-[50px] w-full items-center justify-between bg-white  p-4 2xl:h-[80px]   ">
+    <div className=" fixed z-50 flex h-[50px]  w-full  items-center justify-between bg-white p-4  shadow-sm 2xl:h-[80px]   ">
       <Link
         to="/"
-        className="z-1000 cursor-pointer text-lg font-bold 2xl:text-3xl "
+        className="cursor-pointer text-lg font-bold duration-500  hover:text-orange-500 2xl:text-3xl "
         onClick={() => (window.location.href = "/")}
       >
         NHAT
       </Link>
       <div className="hidden md:block 2xl:text-xl">
-        <Link to="/" className="mx-4">
+        <Link to="/" className="mx-4 duration-500  hover:text-orange-500">
           Home
         </Link>
-        <Link to="/about" className="mx-4">
+        <Link to="/about" className="mx-4  duration-500 hover:text-orange-500">
           About
         </Link>
-        <Link to="/projects" className="mx-4">
+        <Link
+          to="/projects"
+          className="mx-4 duration-500 hover:text-orange-500"
+        >
           Projects
         </Link>
-        <Link to="/contact" className="mx-4">
+        <Link to="/contact" className="mx-4 duration-500 hover:text-orange-500">
           Contact
         </Link>
       </div>
