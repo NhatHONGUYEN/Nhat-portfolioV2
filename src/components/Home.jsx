b1f40ca7e83c26d1597e3a0e9d9d0b38a052dcd1
NhatHONGUYEN <hnnhat67@gmail.com>
2024-05-28 18:09:54 +0200

    ✨ F03 : SECTION > Animation for reveal

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,15 +3,26 @@ import Contact from "./Contact";
 import Introduction from "./Introduction";
 import Projects from "./projects/Projects";
 import Footer from "./footer/Footer";
+import Section from "../animation/Section";
 
 export default function Home() {
   return (
     <>
-      <Introduction />
-      <About />
-      <Projects />
-      <Contact />
-      <Footer />
+      <Section>
+        <Introduction />
+      </Section>
+      <Section>
+        <About />
+      </Section>
+      <Section>
+        <Projects />
+      </Section>
+      <Section>
+        <Contact />
+      </Section>
+      <Section>
+        <Footer />
+      </Section>
     </>
   );
 }
