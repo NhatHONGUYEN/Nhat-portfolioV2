7175677ff49ab035798062f65ede6aac04e13c6c
NhatHONGUYEN <hnnhat67@gmail.com>
2024-05-28 09:41:07 +0200

    💄 F02 : Change the structure of the layout for more responsive later

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,18 +1,22 @@
 export default function About() {
   return (
-    <div className="h-full w-full">
-      <div className="  block h-full   w-full  pt-10 lg:flex  lg:h-screen lg:items-center lg:justify-center  xl:px-40   2xl:ml-[15%]  2xl:w-[70%] 2xl:gap-10 2xl:pt-0  ">
+    <div className="relative bg-gray-900">
+      <div className="relative h-80 overflow-hidden  md:absolute md:left-0 md:h-full md:w-1/3 lg:w-1/2">
         <img
-          className="mx-auto h-full w-full transform transition  sm:w-1/2   lg:ml-20 lg:h-[500px]  lg:w-1/2 2xl:ml-0  2xl:h-[500px] 2xl:w-auto "
-          src="/photo-profil.jpg"
-          alt="photo-profil"
+          className="h-full w-full object-contain"
+          src="../../public/photo-profil.jpg"
+          alt=""
         />
-        <div className="m-10 block items-center justify-center text-center sm:mx-40 lg:mx-20 2xl:mx-auto 2xl:w-[50%]  ">
-          <h2 className="py-4 text-xl font-bold 2xl:text-2xl">ABOUT ME</h2>
-          <h1 className="text-2xl font-bold 2xl:text-3xl">
+      </div>
+      <div className="relative mx-auto max-w-7xl py-24 sm:py-32 lg:px-8 lg:py-40">
+        <div className="pl-6 pr-6 md:ml-auto md:w-2/3 md:pl-16 lg:w-1/2 lg:pl-24 lg:pr-0 xl:pl-32">
+          <h2 className="text-base font-semibold leading-7 text-indigo-400">
+            ABOUT ME
+          </h2>
+          <p className="mt-2 text-3xl font-bold tracking-tight text-white sm:text-4xl">
             Front-end Développeur basé sur Paris , France
-          </h1>
-          <p className="py-10  2xl:text-xl">
+          </p>
+          <p className="mt-6 text-base leading-7 text-gray-300">
             En reconversion professionnelle, je suis passionné par le
             développement front-end et motivé pour apprendre et maîtriser les
             technologies web telles que HTML, CSS, JavaScript et React. Je suis
