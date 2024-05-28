0a9971569f36dcbaccb48092fa6a0b896c8f19c8
NhatHONGUYEN <hnnhat67@gmail.com>
2024-05-28 09:28:44 +0200

    ♻️ F02 : export Features for less code in the same component

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,29 +1,4 @@
-const features = [
-  {
-    name: "Crazy-Burger",
-    description:
-      "Petit projet de site web via la formation de viDev. Le but était de faire mes premier pas en REACT et de comprendre les bases de ce framework.",
-    imageSrc: "/crazee-burger.png",
-    imageAlt: "website crazy-burger.",
-    href: "https://crazee-burger-eosin-rho.vercel.app/",
-  },
-  {
-    name: "Sushi-Sensation",
-    description:
-      "Site web me permettant de me familiariser avec le framework REACT. Je me suis inspiré d'un template réalisé sur figma . Le but était de pouvoir intégrer une maquette individuellement. J'ai surtout approfondi mes connaissance en css avec style-component et pu faire des animations en utilisant react-transition-group.",
-    imageSrc: "/Sushi-sensation.png",
-    imageAlt: "website sushi-sensation.",
-    href: "https://sushi-sensation.vercel.app/",
-  },
-  {
-    name: "Netflix-Clone",
-    description:
-      "Exercice de clone avec NETFLIX. La partie dont j'ai voulu approfondir était la partie back-end. J'ai donc utilisé Firebase pour la gestion de la base de donnée et le stockage des images. Le but était de faire pouvoir se login et de pouvoir retrouvé cest titre préféré.",
-    imageSrc: "/netflix-port.png",
-    imageAlt: "website Netflix-clone.",
-    href: "https://netflix-clone-delta-gray.vercel.app/",
-  },
-];
+import { Features } from "./Features";
 
 export default function Projects() {
   return (
@@ -42,7 +17,7 @@ export default function Projects() {
           </div>
 
           <div className="mt-10 space-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16">
-            {features.map((feature) => (
+            {Features.map((feature) => (
               <div
                 key={feature.name}
                 className="flex flex-col-reverse lg:grid lg:grid-cols-12 lg:items-center lg:gap-x-8"
